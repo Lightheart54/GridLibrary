db48bbf0f89f5a66f37016d2b5a9cbaf634962c9
Cullen Sarles <cullen.sarles@gmail.com>
2016-12-24 11:52:08 -0800

    Add GridRadius and GridFrequency Properties

--- GridLibraryProject/Source/LightWeightGridLibrary/Private/GeodesicGrid.cpp
+++ GridLibraryProject/Source/LightWeightGridLibrary/Private/GeodesicGrid.cpp
@@ -11,7 +11,8 @@ UGeodesicGrid::UGeodesicGrid()
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = false;
-
+	GridFrequency = 0;
+	GridRadius = 100.0f;
 	// ...
 }
 
@@ -52,5 +53,45 @@ int32 UGeodesicGrid::GetLocationIndex_Implementation(const FVector& location) co
 TArray<int32> UGeodesicGrid::GetIndexNeighbors_Implementation(int32 gridIndex) const
 {
 	return TArray<int32>();
+}
+
+int32 UGeodesicGrid::GetGridFrequency() const
+{
+	return GridFrequency;
+}
+
+void UGeodesicGrid::SetGridFrequency(int32 newFrequency)
+{
+	GridFrequency = newFrequency;
+	buildGrid();
+}
+
+float UGeodesicGrid::GetGridRadius() const
+{
+	return GridRadius;
+}
+
+void UGeodesicGrid::SetGridRadius(float newRadius)
+{
+	GridRadius = newRadius;
+	buildGrid();
+}
+
+void UGeodesicGrid::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	FName PropertyName = (PropertyChangedEvent.Property != NULL) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+	if ((PropertyName == GET_MEMBER_NAME_CHECKED(UGeodesicGrid, GridFrequency)) || (PropertyName == GET_MEMBER_NAME_CHECKED(UGeodesicGrid, GridRadius)))
+	{
+		buildGrid();
+	}
+	else
+	{
+		Super::PostEditChangeProperty(PropertyChangedEvent);
+	}
+}
+
+void UGeodesicGrid::buildGrid()
+{
+	
 }
 
--- GridLibraryProject/Source/LightWeightGridLibrary/Public/GeodesicGrid.h
+++ GridLibraryProject/Source/LightWeightGridLibrary/Public/GeodesicGrid.h
@@ -22,20 +22,60 @@ public:
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
+	// See ILightWeightGridInterface::GetNumbeOfElements
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Gridding")
 		int32 GetNumberOfElements() const;
 		virtual int32 GetNumberOfElements_Implementation() const override;
 
+	// See ILightWeightGridInterface::GetIndexLocation
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Gridding")
 		FVector GetIndexLocation(int32 gridIndex) const;
 		FVector GetIndexLocation_Implementation(int32 gridIndex) const override;
 
+	// See ILightWeightGridInterface::GetLocationIndex
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Gridding")
 		int32 GetLocationIndex(const FVector& location) const;
 		int32 GetLocationIndex_Implementation(const FVector& location) const override;
 
+	// See ILightWeightGridInterface::GetIndexNeighbors
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Gridding")
 		TArray<int32> GetIndexNeighbors(int32 gridIndex) const;
 		TArray<int32> GetIndexNeighbors_Implementation(int32 gridIndex) const override;
 	
+	/* Gets the Grid Frequency*/
+	UFUNCTION(BlueprintCallable, Category = "Gridding")
+		int32 GetGridFrequency() const;
+
+	/* Sets the Grid Frequency.
+	 * This rebuilds the grid, so any prior references to it are now invalid
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Gridding")
+		void SetGridFrequency(int32 newFrequency);
+
+	/* Gets the Grid Radius*/
+	UFUNCTION(BlueprintCallable, Category = "Gridding")
+		float GetGridRadius() const;
+
+	/* Sets the Grid Radius.
+	* This rebuilds the grid, so any prior references to it are now invalid
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Gridding")
+		void SetGridRadius(float newRadius);
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+protected:
+	/* The Number of Vertexes Between the Base Icosahedron Vertexes */
+	UPROPERTY(EditAnywhere, Category = "Gridding", meta = (ClampMin = 0, UIMin = 0))
+		int32 GridFrequency;
+
+	/* The Radius of the Grid's Circumscribing Sphere*/
+	UPROPERTY(EditAnywhere, Category = "Gridding", meta = (ClampMin = 0.0, UIMin = 0.0))
+		float GridRadius;
+
+private:
+	/* Constructs a Geodesic Grid With the Specified Frequency */
+	void buildGrid();
 };
